1e66a69af160173b36e789287732aa469d8dedc4
KJ322 <jacksonk3717@student.faytechcc.edu>
2025-01-15 18:47:00 +0000

    Learning how to use codespaces for c++

---
+++ .vscode/hello_world.cpp
@@ -0,0 +1,14 @@
+/* CSC134
+Hello world
+Katherine Jackson
+01/15/25 */
+
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+   cout << "I wish I'd gotten to watch the Firefly Blue Ghost mission launch last night." << endl;
+   return 0;
+}
