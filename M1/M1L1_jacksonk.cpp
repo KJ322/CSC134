9fffca1c01e856ad52d2daad06b179c01ff3865d
KJ322 <jacksonk3717@student.faytechcc.edu>
2025-01-27 19:15:11 +0000

    m1l1 half done

--- M1/M1L1_jacksonk.cpp
+++ M1/M1L1_jacksonk.cpp
@@ -16,9 +16,9 @@ int main()
     int num_apples = 100;
     double price_each = 0.25;
 
-   cout << "Welcome to the " << name << " apple farm." << endl;
+   cout << "Welcome to the " << name << " Apple Farm." << endl;
    cout << "We have " << num_apples << " in stock." << endl;
-   cout << "The price per apple is $ " << price_each << endl;
+   cout << "The price per apple is $" << price_each << endl;
    cout << endl;
    return 0;
 }
