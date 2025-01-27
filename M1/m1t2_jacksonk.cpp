8e709c0b85c8a349b145c19b7dbce97f2efb1d29
KJ322 <jacksonk3717@student.faytechcc.edu>
2025-01-27 18:40:35 +0000

    added more cout messages

---
+++ M1/m1t2_jacksonk.cpp
@@ -0,0 +1,17 @@
+/* CSC134
+Hello world
+Katherine Jackson
+01/27/25 */
+
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+   cout << "Hello" << endl;
+   cout << "I love all things space and science related." << endl;
+   cout << "I love listening to and making music." << endl;
+   cout << "I also love traveling and learning about other cultures and historic events." << endl;
+   return 0;
+}
