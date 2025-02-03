815b428cf7893f09d62bced29204e9220d0bc651
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-02-03 14:29:03 -0500

    saving turtle demo

---
+++ demo_jacksonk.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+t = Turtle()
+t.pensize(5)
+t.pencolor("blue")
+
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.end_fill()
