82ceb4b6174b8cd0bc2d953f10098098482ca3fb
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-02-03 14:34:17 -0500

    updated turtle demo file

--- demo_jacksonk.py
+++ demo_jacksonk.py
@@ -3,14 +3,15 @@ from turtle import *
 t = Turtle()
 t.pensize(5)
 t.pencolor("blue")
+t.fillcolor("orange")
+t.shape("turtle")
 
 t.begin_fill()
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
+
+for side in [1,2,3,4]:
+    t.forward(100)
+    t.left(90)
+
+t.begin_fill()
+t.circle(360)
 t.end_fill()
