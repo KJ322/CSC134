a5cb64913a1806304b45bf7442c9e7830b104720
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-02-24 19:09:06 +0000

    edited m3t2

--- M3/m3t2_jacksonk.cpp
+++ M3/m3t2_jacksonk.cpp
@@ -14,25 +14,26 @@ Jackson K
 using namespace std;
 
 int main() {
-    const int SIDES = 10; // yours will differ!
-    const int DECIEVE = 60;
+    const int SIDES = 20; // yours will differ!
+    const int CHECK = 15;
     //int seed = 42;
     int seed = time(0);
     //cout << "seed is: " << seed << endl;
     // seed the random number generator
     srand(seed);
     // rand() is a large number, so we take the remainder which is %
-    int roll1, roll2, total;
+    int roll1, modifier, total;
     // roll a few times
     roll1 = ( (rand() % SIDES)+1 );
     cout << roll1 << endl;
-    roll2 = ( (rand() % SIDES)+1 );
-    cout << roll2 << endl;
 
-    total = (roll1 * 10) + roll2;
-    cout << "You rolled " << (roll1 * 10) << " + " << roll2 << " = " << total << endl;
+    //roll2 = ( (rand() % SIDES)+1 );
+    //cout << roll2 << endl;
 
-    if (total >= DECIEVE)
+    total = roll1 + modifier;
+    cout << "You rolled " << roll1 << " + " << modifier << " = " << total << endl;
+
+    if (total >= CHECK)
     {
       cout << "You succeeded!" << endl;
     }
