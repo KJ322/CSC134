6fb59b2f273b0a05ad80eddc3199eb420aa89b0a
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-09 21:11:55 +0000

    started m3hw q3

---
+++ M3/m3hw_JacksonK_Q3.cpp
@@ -0,0 +1,24 @@
+/*
+CSC134
+M3HW - gold (Q3 CYOA)
+Jackson K
+03/09/25
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+  //declaring variables
+  int choice;
+
+  //retreiving user input
+  
+
+  
+  
+
+  return 0;
+}
