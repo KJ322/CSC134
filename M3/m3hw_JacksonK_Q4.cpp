912858c1ce91d4c94440e67c1c4e971b123107da
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-09 21:37:16 +0000

    started m3hw q4

---
+++ M3/m3hw_JacksonK_Q4.cpp
@@ -0,0 +1,40 @@
+/*
+CSC134
+M3HW - gold (Q4 Math practice)
+Jackson K
+03/09/25
+*/
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() 
+{
+  //declaring variables
+  int seed = time(0);
+  int answer;
+
+  //seeding random number generator
+  srand(seed);
+
+  //declaring rest of variables
+  int num1 = rand();
+  int num2 = rand();
+
+  cout << "Solve this problem: " << endl;
+  cout << num1 << " + " << num2 << endl;
+  cin >> answer;
+
+  //determines whether answer is right
+  if (answer == num1 + num2)
+  {
+    cout << "Correct!" << endl;
+  }
+  else
+  {
+    cout << "Incorrect" << endl;
+  }
+
+  return 0;
+}
