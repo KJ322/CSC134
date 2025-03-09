93da838eff7878ec831e5ac3254fa75693b06b5f
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-09 20:19:12 +0000

    started m3lab2

---
+++ M3/m3lab2.cpp
@@ -0,0 +1,48 @@
+/*
+CSC134
+M3LAB2 - Letter grade
+Jackson K
+03/09/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  //declaring variables
+  int grade;
+  char letter_grade;
+
+  //getter numerical grade from user
+  cout << "Enter numerical grade: " << endl;
+  cin >> grade;
+
+  //determines letter grade based on user input
+  if (grade >= 90 && grade <= 100)
+  {
+    letter_grade = 'A';
+  }
+  else if (grade >= 80)
+  {
+    letter_grade = 'B';
+  }
+  else if (grade >= 70)
+  {
+    letter_grade = 'C';
+  }
+  else if (grade >= 60)
+  {
+    letter_grade = 'D';
+  }
+  else if (grade < 60)
+  {
+    letter_grade = 'F';
+  }
+  else if (grade > 100 || grade < 0)
+  {
+    cout << "Invalid grade. Try again." << endl;
+  }
+
+  return 0;
+}
