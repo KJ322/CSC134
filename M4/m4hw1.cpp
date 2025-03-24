625de68bcac903ade7e9c45802dda0b86a1e8d06
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-24 18:05:22 +0000

    started m4hw1

--- M4/m4hw1.cpp
+++ M4/m4hw1.cpp
@@ -8,10 +8,46 @@ Katherine Jackson
   
 using namespace std;
 
-
+void print_line(int number, int product, int user_number);
+int multiply(int number, int user_number);
+int get_num(int user_number);
 
 int main()
 {
+    int user_number = get_num(user_number);
+
+    for (int num = 1; num <= 12; num++)
+    {
+        int product = multiply(num, user_number);
+        print_line(num, product, user_number);
+    }
 
     return 0;
+}
+
+int get_num(int user_number)
+{
+    cout << "Enter a number between 1 and 12: ";
+    cin >> user_number;
+
+    if (user_number < 1 || user_number > 12)
+    {
+        cout << "Number is not between 1 and 12. Please try again." << endl;
+        cout << "Enter a number between 1 and 12: ";
+        cin >> user_number;
+    }
+
+    return user_number;
+}
+
+int multiply (int number, int user_number)
+{
+    int product = user_number * number;
+
+    return product;
+}
+
+void print_line(int number, int product, int user_number)
+{
+    cout << user_number << " times " << number << " is " << product << endl;
 }
--- M5/m5t2.cpp
+++ M5/m5t2.cpp
@@ -8,7 +8,28 @@ Katherine Jackson
   
 using namespace std;
 
+int find_square(int number);
+void print_line(int number, int square);
+
 int main()
 {
-    
+    for (int num = 1; num <= 10; num++)
+    {
+        int square = find_square(num); //should call find_square()
+        print_line (num, square);
+    }
+
+    return 0;
+}
+
+int find_square(int number)
+{
+    int square = number * number;
+
+    return square;
+}
+
+void print_line(int number, int square)
+{
+    cout << "The square of " << number << " is " << square << endl;
 }
